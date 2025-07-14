755ce498882d8c2f7151bcad5fd619fa15401ce0
AulixPlan <aulixplanejamento@gmail.com>
2025-07-14 04:53:00 -0300

    Update PricingSection.tsx

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -48,10 +48,6 @@ export const PricingSection = () => {
                   <Check className="w-5 h-5 text-secondary flex-shrink-0" />
                   <span>Todos os tipos de documentos</span>
                 </li>
-               <!--  <li className="flex items-center gap-2">
-                  <Check className="w-5 h-5 text-secondary flex-shrink-0" />
-                  <span>Suporte via WhatsApp</span>
-                </li> -->
                 <li className="flex items-center gap-2">
                   <Check className="w-5 h-5 text-secondary flex-shrink-0" />
                   <span>Base BNCC</span>
@@ -149,10 +145,6 @@ export const PricingSection = () => {
                   <Check className="w-5 h-5 text-secondary flex-shrink-0" />
                   <span>Acesso antecipado às novidades</span>
                 </li>
-                <!-- <li className="flex items-center gap-2">
-                  <Check className="w-5 h-5 text-secondary flex-shrink-0" />
-                  <span>Consultoria pedagógica</span>
-                </li> -->
               </ul>
               <Button 
                 className="w-full" 
